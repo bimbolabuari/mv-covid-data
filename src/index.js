53a47a11cb7ac5df60eff73e2169d70cf3bf8d62
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-09-10 16:57:39 +0100

    feat: Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import store from './redux/configureStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
+
 );
