98b87e8f7eadaf6e0b2dff8a6ffbd9a22263547f
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-09-10 16:51:27 +0100

    feat: Add home component page

---
+++ src/components/Home.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import getData from '../actions/actionCreator';
+import microphone from '../images/microphone.png';
+import leftArrow from '../images/left-arrow.png';
+import right from '../images/right.png';
+import settings from '../images/settings.png';
+
+const Home = () => {
+  const covidData = useSelector((data) => data.Reducer);
+  const dispatch = useDispatch();
+  const date = '2020-10-10';
+  const APIURL = `https://api.covid19tracking.narrativa.com/api/${date}`;
+  useEffect(() => {
+    const covidApi = async () => {
+      const fetchAPI = await fetch(APIURL);
+      const covidData = await fetchAPI.json();
+      const data = covidData.dates[date].countries;
+      const newData = Object.values(data);
+      return dispatch(getData(newData));
+    };
+    covidApi();
+  }, []);
+
+  return (
+    <div className="home-container">
+      <div className="flex header">
+        <div className="flex">
+          <img className="images" alt="arrow" src={leftArrow} />
+          <p>2020</p>
+        </div>
+        <div>
+          <p>most views</p>
+        </div>
+        <div>
+          <img className="images" alt="record" src={microphone} />
+          <img className="images" alt="settings" src={settings} />
+        </div>
+      </div>
+      <div className="mainCity">
+        <h1 className="mostViewed">Covid19 Tracker</h1>
+        <h2>365 countries</h2>
+      </div>
+      <div>
+        <h2 className="stats-title">STATS BY COUNTRY</h2>
+        <div className="flex stat-container">
+          { covidData.map(({ id, name, today_confirmed: todayConfirmed }) => (
+            <NavLink to={`/details/${id}`} className="navLink stats" key={id}>
+              <img className="images" alt="pointer" src={right} />
+              <div>
+                <p>{name}</p>
+                <p>{todayConfirmed}</p>
+              </div>
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
